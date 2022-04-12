d14d46c2ba2d73daebef39ad3dc13670e75ab7dd
Sakha <jenderwd@gmail.com>
2022-04-12 22:52:31 +0300

    main page styled

--- pin_api.py
+++ pin_api.py
@@ -1,6 +1,9 @@
+import datetime
+
 from flask import Blueprint, render_template, redirect
 from data.db_session import create_session
 from data.pin import Pin
+from data.user import User
 from flask_login import current_user, mixins
 
 
@@ -21,6 +24,9 @@ def print_pins():
     for pin in pins:
         pins1.append({'mem': f'static/img/mem{pin.id}.jpg',
                       'alt': pin.alt,
-                      'id': pin.id})
-    return render_template('pins.html', title='Все мемы', pins=pins, current_user=curus)
+                      'id': pin.id,
+                      'title': pin.title,
+                      'author': db_sess.query(User).filter(User.id == pin.user_id)[0]
+                      })
+    return render_template('pins.html', title='Все мемы', pins=pins1, current_user=curus)
 
